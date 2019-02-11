3b18bea744a27d449e6eb4e461a4fb6f549c7459
Jonathan Berkowitz <jonrberkowitz@gmail.com>
2019-02-11 19:38:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Code your solution here:
 function driversWithRevenueOver(drivers, revenue) {
-  return drivers.filter(function(driver_rev) {
-    return driver_rev > revenue;
+  return drivers.filter(function(driver) {
+    return driver.revenue > revenue;
   });
 }
